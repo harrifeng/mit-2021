e9dba3cc08cd87b9ea4d3063b4ebce94daee4431
Harri Feng <harrifeng@gmail.com>
2021-10-20 15:10:18 +0800

    Lab-1 PASSED ALL TESTS

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -23,7 +25,25 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
+type HandlerArgs struct {
+}
 
+type HandlerReply struct {
+	JobType     string
+	MapFile     string
+	MapIndex    int
+	MapNum      int
+	ReduceIndex int
+	ReduceNum   int
+}
+
+type NotifyArgs struct {
+	NotifyType  string
+	NotifyIndex int
+}
+
+type NotifyReply struct {
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
